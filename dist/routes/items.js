ff7e2509d6ae617de815bfb726a0ababe39bd502
Mike Lambert <mikelambert@mikebook.local>
2014-05-05 19:52:16 -0400

    Different organization for server routes

--- dist/routes/items.js
+++ dist/routes/items.js
@@ -1,12 +1,21 @@
 var mongo = require('mongodb');
  
-var Server = mongo.Server,
+var Connection = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
  
-var server = new Server('localhost', 27017, {auto_reconnect: true});
-db = new Db('itemdb', server);
- 
+var db = new Db('itemdb', new Connection('localhost', 27017, {
+    auto_reconnect: true, 
+    journal: true, 
+    safe: false
+}));
+
+db.on('disconnected', function() {
+    console.log('MongoDB disconnected');
+}).on('error', function(err) {
+    console.log('MongoDB error :( ' + err);
+});
+
 db.open(function(err, db) {
     if(!err) {
         console.log("Connected to 'itemdb' database");
@@ -17,70 +26,101 @@ db.open(function(err, db) {
         });
     }
 });
- 
-exports.findById = function(req, res) {
-    var id = req.params.id;
-    console.log('Retrieving item: ' + id);
-    db.collection('items', function(err, collection) {
-        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-            res.send(item);
-        });
-    });
-};
- 
-exports.findAll = function(req, res) {
-    db.collection('items', function(err, collection) {
-        collection.find().toArray(function(err, items) {
-            res.send(items);
-        });
-    });
-};
- 
-exports.addItem = function(req, res) {
-    var item = req.body;
-    console.log('Adding item: ' + JSON.stringify(item));
-    db.collection('items', function(err, collection) {
-        collection.insert(item, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
-                res.send(result[0]);
-            }
-        });
-    });
-}
- 
-exports.updateItem = function(req, res) {
-    var id = req.params.id;
-    var item = req.body;
 
-    delete item._id; // http://stackoverflow.com/questions/14585507/mongodb-cant-update-document-because-id-is-string-not-objectid
-    
-    db.collection('items', function(err, collection) {
-        collection.update({'_id': new BSON.ObjectID(id)}, item, {safe:true}, function(err, result) {
-            if (err) {
-                console.log('Error updating item: ' + err);
-                res.send({'error':'An error has occurred upading item ' + id});
-            } else {
-                console.log('' + JSON.stringify(item) + ' updated');
+
+module.exports = function(app){
+
+    this.findById = function(req, res) {
+        var id = req.params.id;
+        console.log('Retrieving item: ' + id);
+
+        db.collection('items', function(err, collection) {
+            collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
                 res.send(item);
-            }
+            });
         });
-    });
-}
+    };
+     
+    this.findAll = function(req, res) {
+        db.collection('items', function(err, collection) {
+            collection.find().toArray(function(err, items) {
+                res.send(items);
+            });
+        });
+    };
+     
+    this.addItem = function(req, res) {
+        console.log("REQUEST: ", req);
+        var item = req.body;
+        console.log('Adding item: ' + JSON.stringify(item));
+
+        db.collection('items', function(err, collection) {
+            collection.insert(item, {safe:true}, function(err, result) {
+                if (err) {
+                    console.log('Error :( ', err);
+                    res.send({
+                        'error': 'An error has occurred :('
+                    });
+                } else {
+                    // console.log('Success!');
+                    res.send(result[0]);
+                }
+            });
+        });
+    }
+     
+    this.updateItem = function(req, res) {
+        var id = req.params.id;
+        var item = req.body;
+
+        delete item._id; // http://stackoverflow.com/questions/14585507/mongodb-cant-update-document-because-id-is-string-not-objectid
+        
+        console.log('Updating item: ' + JSON.stringify(item));
+
+        db.collection('items', function(err, collection) {
+            collection.update({'_id': new BSON.ObjectID(id)}, item, {safe:true}, function(err, result) {
+                if (err) {
+                    console.log('Error :( ', err);
+                    res.send({
+                        'error': 'An error has occurred :('
+                    });
+                } else {
+                    // console.log('Success!');
+                    res.send(item);
+                }
+            });
+        });
+    }
+     
+    this.deleteItem = function(req, res) {
+        var id = req.params.id;
+        console.log('Deleting item: ' + id);
+
+        db.collection('items', function(err, collection) {
+            collection.remove({'_id': new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+                if (err) {
+                    console.log('Error :( ', err);
+                    res.send({
+                        'error': 'An error has occurred'
+                    });
+                } else {
+                    // console.log('Success! ' + result + ' document' + ((result > 1) ? 's' : '') + ' deleted');
+                    res.send(req.body);
+                }
+            });
+        });
+    }
+
+    app.get('/items', this.findAll);
+    app.get('/items/:id', this.findById);
+    app.post('/items', this.addItem);
+    app.put('/items/:id', this.updateItem);
+    app.delete('/items/:id', this.deleteItem);
+
+
+
+}; // module.exports
+
  
-exports.deleteItem = function(req, res) {
-    var id = req.params.id;
-    console.log('Deleting item: ' + id);
-    db.collection('items', function(err, collection) {
-        collection.remove({'_id': new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred - ' + err});
-            } else {
-                console.log('' + result + ' document(s) deleted');
-                res.send(req.body);
-            }
-        });
-    });
-}
+
+
